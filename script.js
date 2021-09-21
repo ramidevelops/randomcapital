df595588fa69223324511f5995da2addea8aafb9
ramon <85133666+ramidevelops@users.noreply.github.com>
2021-09-20 21:30:25 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -18,27 +18,33 @@ const chart = LightweightCharts.createChart(tvChart, chartProperties);
 
 const candleSeries = chart.addBarSeries();
 
-let price;
-
-fetch('https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1d&limit=1000')
-.then(res => res.json())
-.then(data => {
-
-
-    const mappedData = data.map( d =>{
-        price = parseFloat(data[999][4]).toFixed(2)
-            return {time: d[0]/1000, open: parseFloat(d[1]), high: parseFloat(d[2]), low: parseFloat(d[3]), close: parseFloat(d[4])};
-    })
+const getChart = async () =>{
     
-    candleSeries.setData(mappedData)
-}).catch(err => console.log(err));
+    try{
+        const response = await fetch('https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1d&limit=1000')
+        if(response.ok){
+            const jsonResponse = await response.json();
+            const mappedData = await jsonResponse.map( d =>{
+                return {time: d[0]/1000, open: parseFloat(d[1]), high: parseFloat(d[2]), low: parseFloat(d[3]), close: parseFloat(d[4])};
+                })
+            price = parseFloat(mappedData[999].close).toFixed(2)
+            
+            candleSeries.setData(mappedData)}
+
+        }   catch(err){
+            console.log(err)
+        }
+    };
+
 
 //EMA
 
 
-    const emaDom = document.getElementById('emavalue');
+    const emaDom = document.getElementById('emadiv');
     const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhbWlkZXZlbG9wc0BnbWFpbC5jb20iLCJpYXQiOjE2MzE2NjAwMDIsImV4cCI6NzkzODg2MDAwMn0.ta682GMYRX6brX2AKB4sI1fM5OUfhhZmItW4QKZ8eDU'
     const urlToFetch = `https://api.taapi.io/ema?secret=${apiKey}&exchange=binance&symbol=ETH/USDT&interval=1d&optInTimePeriod=50`;
+
+    let emavalue;
 
     const getEma = async ()=>{
 
@@ -46,11 +52,34 @@ fetch('https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1d&limit=10
         const response = await fetch(urlToFetch)
         if(response.ok){
             const jsonResponse = await response.json();
-            emaDom.innerHTML = jsonResponse.value.toFixed(2);//`<br>${price}`;
+            emavalue = jsonResponse.value.toFixed(2);
+            emaDom.innerHTML = jsonResponse.value.toFixed(2);
         }
     }catch(err){
         console.log(err)
     }
+};
+
+
+const comparison =(price, emaPrice) =>{
+
+
+    const comparisonDom = document.getElementById('comparison');
+
+
+    if(price<emaPrice){
+        comparisonDom.innerHTML = `assets actual price is <span style="color:red"><b>below</b></span> the 50EMA. A downtrend is expected. Play at your own risk.`
+    } else {
+        comparisonDom.innerHTML = `assets actual price is <span style="color:green"><b>above</b></span> the 50EMA. Asset should be longed. Keep risk management in mind.`
+    }
+    
+};
+
+
+const execute = async()=>{
+    await getChart();
+    await getEma();
+    comparison(price, emavalue);
 }
 
-getEma();
+execute();
